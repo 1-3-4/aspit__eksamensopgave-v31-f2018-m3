5bf8942e9d22ef44838ddb702e7e364db531bdf3
134 <38720563+1-3-4@users.noreply.github.com>
2018-06-28 15:29:10 +0200

    Began adding js functionality to the site.
    Created the functionality to add columns to the DOM.

--- src/js/_test.js
+++ src/js/_test.js
@@ -1,4 +1,19 @@
-const hello = (name) => {
-    return 'hello ' + name + '!';
-    // return 'hello ${name}';
-}
+// const hello = (name) => {
+//     return 'hello ' + name + '!';
+//     // return 'hello ${name}'; // Why does this not work?
+// }
+// var nameBuilder = function (firstName="John", lastName="Doe") {
+//     console.log(firstName + " " + lastName);
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+    // VARIABLES
+    const cardCatalog = document.getElementById('card-catalog');
+    const col = document.createElement('div');//.classList.add('col-sm-6', 'py-3');
+    
+    // col.innerHTML('Extra column!');
+    col.classList.add('col-sm-6');
+    col.classList.add('py-3');
+    cardCatalog.appendChild(col);
+    console.log(col);
+});
