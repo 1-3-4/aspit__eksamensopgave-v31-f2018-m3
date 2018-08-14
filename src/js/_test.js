70a26b2eb377db3ff62a5b3674ac5926e1389ed0
134 <38720563+1-3-4@users.noreply.github.com>
2018-08-14 12:02:29 +0200

    Added method to JS class.
    render() makes it posible to insert the object into the DOM styles as an order card.

--- src/js/_test.js
+++ src/js/_test.js
@@ -9,39 +9,39 @@
 document.addEventListener("DOMContentLoaded", function () {
     // VARIABLES
     const cardCatalog = document.getElementById('card-catalog');
+    // JavaScript object, not correct JSON ???
     const products = {
         "coffee1": {
             "navn" : "Americano",
             "beskrivelse" : "Stærk crema espresso med varmt vand",
-            "pris" : "60kr",
-            "img" : "Americano.jpg"
+            "pris" : 60,
+            "img" : "americano.jpg"
         },
         "coffee2": {
             "navn" : "Caffe latte",
             "beskrivelse" : "Espresso med skummet varm mælk",
-            "pris" : "65kr",
+            "pris" : 65,
             "img" : "Caffe_Latte.jpg"
         },
         "coffee3": {
             "navn" : "Cappuccino",
             "beskrivelse" : "Espresso med dampet mælk og skum",
-            "pris" : "75kr",
+            "pris" : 75,
             "img" : "Cappucino.jpg"
         },
         "coffee4": {
             "navn" : "Espresso",
             "beskrivelse" : "Espresso lavet af vores dygtigste baristaer",
-            "pris" : "50kr",
+            "pris" : 50,
             "img" : "Espresso.jpg"
         },
         "coffee5": {
             "navn" : "Macchiato",
             "beskrivelse" : "Lækker espressodrik med skummet mælk og chokolade",
-            "pris" : "100kr",
+            "pris" : 100,
             "img" : "Macchiato.jpg"
         }
     }
-    console.log(products.coffee1.navn);
 
     // CLASS(ES)
     class product {
@@ -51,8 +51,35 @@ document.addEventListener("DOMContentLoaded", function () {
             this.description = description;
             this.price = price;
         }
-        createCard(){}
-        addCardToDOM(){}
+        render(){
+            return `<div class="col-sm-6 py-3">
+                        <article class="card h-100">
+                            <img class="card-img-top" src="./img/product__${this.image}">
+                            <div class="card-body">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col px-0">
+                                            <h3 class="card-title">${this.title}</h3>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col px-0">
+                                            <p class="card-text">${this.description}</p>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col px-0">
+                                            <button class="btn btn-outline-secondary my-3">Bestil</button>
+                                        </div>
+                                        <div class="col px-0 d-inline-flex flex-row flex-nowrap justify-content-end align-items-center">
+                                            <p class="card-text">${this.price}kr</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    </div>`;
+        }
     }
 
     // const product1 = new product('americano', 'Americano', 'Stærk crema espresso med varmt vand.', 60);
@@ -60,8 +87,26 @@ document.addEventListener("DOMContentLoaded", function () {
     // const product1 = new product(products.coffee1.img, products.coffee1.navn, products.coffee1.beskrivelse, products.coffee1.pris);
     // console.log(product1);
 
-    forEach(){
+    const keys = Object.keys(products);
+    for (let i = 0; i < keys.length; i++) {
+        // console.log(keys[i]);
 
+        // Create var, set value to object key, data type string:
+        const key = keys[i];
+        // console.log(key);
+
+        const coffee = new product(
+            // Image
+            products[key].img,
+            // Title
+            products[key].navn,
+            // Description
+            products[key].beskrivelse,
+            // Price
+            products[key].pris
+        );
+        console.log(coffee);
+        cardCatalog.insertAdjacentHTML('beforeend', coffee.render());
     }
 
 
